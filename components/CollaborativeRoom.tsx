818b8a96390731609dbb4323879eafe0ea84b8b5
Ryan Micael <ka9ddcgamer@gmail.com>
2024-08-03 15:48:11 -0300

    feat: live features and error log with sentry

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -14,9 +14,9 @@ import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 import { updateDocument } from "@/lib/actions/room.actions";
 import Loader from "./Loader";
+import ShareModal from "./ShareModal";
 
-const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
-  const currentUserType = 'editor';
+const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
 
   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
   const [editing, setEditing] = useState(false);
@@ -100,6 +100,9 @@ const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => 
               </div>
               <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
                 <ActiveCollaborators />
+
+                <ShareModal roomId={roomId} collaborators={users} creatorId={roomMetadata.creatorId} currentUserType={currentUserType}/>
+
                 <SignedOut>
                   <SignInButton />
                 </SignedOut>
@@ -108,7 +111,7 @@ const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => 
                 </SignedIn>
               </div>
             </Header>
-            <Editor />
+            <Editor roomId={roomId} currentUserType={currentUserType} />
           </div>
         </ClientSideSuspense>
       </RoomProvider>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,10 +2,11 @@
 
 import { clerkClient } from "@clerk/nextjs/server"
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
   try {
-    const {data} = await clerkClient.users.getUserList({
+    const {data} = await clerkClient().users.getUserList({
       emailAddress: userIds,
     });
 
@@ -21,5 +22,23 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
     return parseStringify(sortedUsers);
   } catch (error) {
       console.log(`Error fetching users: ${error}`)
+  }
+}
+
+export const getDocumentUsers = async ({ roomId, currentUser, text}: { roomId: string, currentUser: string, text: string}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    const users = Object.keys(room.usersAccesses).filter((email) => email !== currentUser);
+
+    if(text.length) {
+      const lowerCaseText = text.toLowerCase();
+
+      const filteredUsers = users.filter((email: string) => email.toLowerCase().includes(lowerCaseText));
+
+      return parseStringify(filteredUsers);
+    }
+  } catch (error) {
+    console.log(`Error fetching document users: ${error}`);
   }
 }
